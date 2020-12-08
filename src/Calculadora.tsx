3af6b551e1e41ad153e8def376608252ee42bff0
Paulo Roberto <prmorais1302@gmail.com>
2020-12-08 01:31:13 -0300

    Concluindo o uso do serviço da calculadora no componente principal

--- src/Calculadora.tsx
+++ src/Calculadora.tsx
@@ -2,6 +2,16 @@ import React, { useState } from 'react';
 import { Button, Col, Container, Form, Jumbotron, Row } from 'react-bootstrap';
 import './Calculadora.css';
 
+import CalculadoraService from './Calculadora.service';
+const [
+  calcular,
+  concatenarNumero
+  // SOMA,
+  // SUBTRACAO,
+  // MULTIPLICACAO,
+  // DIVISAO
+] = CalculadoraService();
+
 const Calculadora = () => {
   const [txtNumeros, setTxtNumeros] = useState<string>('0');
   const [numero1, setNumero1] = useState<string>('0');
@@ -12,17 +22,50 @@ const Calculadora = () => {
     let resultado = '';
 
     if (operacao === '') {
+      resultado = concatenarNumero(numero1, numero);
+      setNumero1(resultado)
 
     } else {
-
+      resultado = concatenarNumero(numero2, numero);
+      setNumero2(resultado)
     }
 
     setTxtNumeros(resultado);
   }
 
   const definirOperacao = (op: string) => {
-    setTxtNumeros(op);
+    // Caso a operação não tenha sido definida, seta a operação
+    if (operacao === '') {
+      setOperacao(op);
+      return;
+    }
+
+    // Se a operação foi definida e o número2 foi setado, realiza o cálculo
+    if (numero2 !== '') {
+      const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+      setOperacao(op);
+      setNumero1(resultado.toString());
+      setNumero2('')
+      setTxtNumeros(resultado.toString());
+    }
   };
+
+  const acaoCalcular = () => {
+    if (numero2 === '') {
+      return;
+    }
+
+    const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+
+    setTxtNumeros(resultado.toString());
+  };
+
+  const limpar = () => {
+    setTxtNumeros('0');
+    setNumero1('0');
+    setNumero2('');
+    setOperacao('');
+  }
 
   return (
     <Jumbotron
@@ -39,7 +82,7 @@ const Calculadora = () => {
       <Container>
         <Row>
           <Col xs="3">
-            <Button onClick={() => setTxtNumeros('0')} variant="danger">C</Button>
+            <Button onClick={limpar} variant="danger">C</Button>
           </Col>
           <Col xs="9">
             <Form.Control
@@ -102,10 +145,10 @@ const Calculadora = () => {
             <Button onClick={() => adicionarNumero('0')} variant="light">0</Button>
           </Col>
           <Col>
-            <Button  onClick={() => adicionarNumero('.')} variant="light">.</Button>
+            <Button onClick={() => adicionarNumero('.')} variant="light">.</Button>
           </Col>
           <Col>
-            <Button variant="success">=</Button>
+            <Button onClick={acaoCalcular} variant="success">=</Button>
           </Col>
           <Col>
             <Button onClick={() => definirOperacao('+')} variant="warning">+</Button>
